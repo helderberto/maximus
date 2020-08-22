d73d44e1a51c591d6e7446e3954175088ec8b7ca
Helder Burato Berto <helder.burato@gmail.com>
2020-08-22 19:55:08 -0300

    chore: add babel-loader to webpac.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,27 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: {
-    maximus: path.resolve(__dirname, 'src', 'index.js'),
-  },
+  entry: './src/index',
   output: {
-    filename: '[name].js', // maximus.js
+    filename: 'maximus.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'maximus',
     libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      }
+    ]
   },
   plugins: [new CleanWebpackPlugin()],
 }
