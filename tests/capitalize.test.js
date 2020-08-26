b163a5cd511718b0b400c2d89a5e21a0c1be84c8
Helder Burato Berto <helder.burato@gmail.com>
2020-08-25 21:52:15 -0300

    test(capitalize): add test case with camel case

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,8 +1,8 @@
 import { capitalize } from '../src/string'
 
 describe('maximus.capitalize', () => {
-  test('should capitalize the first letter of the text', () => {
-    const word = 'any_test'
-    expect(capitalize(word)).toBe('Any_test')
+  test('should capitalize the first letter of a text', () => {
+    expect(capitalize('any_test')).toBe('Any_test')
+    expect(capitalize('aNy_TeSt')).toBe('Any_test')
   })
 })
