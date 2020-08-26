8d61d5ca502dbd24b28cf70c6ae49d2b6d385f7b
Helder Burato Berto <helder.burato@gmail.com>
2020-08-26 15:07:44 -0300

    chore(jest): ignore node_modules and dist patterns

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', 'dist'],
 }
