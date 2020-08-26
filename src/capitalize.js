9a5834b2b38a3d134f2d624672423c871602e164
Helder Burato Berto <helder.burato@gmail.com>
2020-08-25 22:04:18 -0300

    feat(capitalize): ensure throws an exception
    It will throws an exception if no parameter has been passed

--- src/capitalize.js
+++ src/capitalize.js
@@ -7,7 +7,9 @@
  * const text = capitalize('hElLo')
  * console.log(text); // => 'Hello'
  */
-export default function capitalize(str) {
-  // console.log(str.toLowerCase().chartAt(0).toUpperCase())
+export default function capitalize(str = '') {
+  if (!str) {
+    throw new Error("Can't capitalize an empty string")
+  }
   return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1)
 }
