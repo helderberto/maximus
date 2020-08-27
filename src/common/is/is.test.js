ce68d580a63880c1f52f4c9097c1325ea03b6980
Helder Burato Berto <helder.burato@gmail.com>
2020-08-27 19:40:43 -0300

    test(is): add test to is.String()
    It ensures the value is a String

--- src/common/is/is.test.js
+++ src/common/is/is.test.js
@@ -1,15 +1,17 @@
-import { isString } from '../is'
+import is from '../is'
 
 describe('maximus.is', () => {
-  test('should ensure the value is a string', () => {
-    expect(isString('any_test')).toBeTruthy()
-  })
+  describe('maximus.is.String', () => {
+    test('should ensure the value is a string', () => {
+      expect(is.String('any_test')).toBeTruthy()
+    })
 
-  test('should ensure the value null returns false', () => {
-    expect(isString(null)).toBeFalsy()
-  })
+    test('should ensure the value null returns false', () => {
+      expect(is.String(null)).toBeFalsy()
+    })
 
-  test('should ensure the value undefined returns false', () => {
-    expect(isString(undefined)).toBeFalsy()
+    test('should ensure the value undefined returns false', () => {
+      expect(is.String(undefined)).toBeFalsy()
+    })
   })
 })
