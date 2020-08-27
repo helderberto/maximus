cd6ad2b41f004b81deb437fcccf978c9212323fa
Helder Burato Berto <helder.burato@gmail.com>
2020-08-27 20:35:29 -0300

    feat: is.array() (#8)
    * refactor(is): rename is.String to is.string
    
    Update the name to lowercase to avoid problems with the JavaScript data types
    
    * feat(is): ensures the value is an array
    
    * test(is): ensures the value is an array

--- src/common/is/index.js
+++ src/common/is/index.js
@@ -1,12 +1,22 @@
 /**
- * @description Validate if str is a String.
- * @param {string} str - Value to be validate.
- * @return {boolean} - Returns true if it's a String.
+ * @description Check if type is a String
+ * @param {String} str - Value to be validate.
+ * @return {Boolean} - Returns true if it's a String.
  */
-function String(str) {
+function string(str) {
   return str && typeof str === 'string'
 }
 
+/**
+ * @description Check if value is an Array
+ * @param {Array} arr - Value to be validate.
+ * @returns {Boolean} - Returns true if it's an Array.
+ */
+function array(arr) {
+  return Array.isArray(arr)
+}
+
 export default {
-  String,
+  string,
+  array,
 }
--- src/common/is/is.test.js
+++ src/common/is/is.test.js
@@ -3,15 +3,41 @@ import is from '../is'
 describe('maximus.is', () => {
   describe('maximus.is.String', () => {
     test('should ensure the value is a string', () => {
-      expect(is.String('any_test')).toBeTruthy()
+      expect(is.string('any_test')).toBeTruthy()
     })
 
     test('should ensure the value null returns false', () => {
-      expect(is.String(null)).toBeFalsy()
+      expect(is.string(null)).toBeFalsy()
     })
 
     test('should ensure the value undefined returns false', () => {
-      expect(is.String(undefined)).toBeFalsy()
+      expect(is.string(undefined)).toBeFalsy()
+    })
+  })
+
+  describe('maximus.is.array', () => {
+    test('should ensure the value is an Array', () => {
+      expect(is.array([])).toBeTruthy()
+    })
+
+    test('given an invalid array', () => {
+      expect(is.array(null)).toBeFalsy()
+    })
+
+    test('given an invalid array', () => {
+      expect(is.array(undefined)).toBeFalsy()
+    })
+
+    test('given an invalid array', () => {
+      expect(is.array()).toBeFalsy()
+    })
+
+    test('given an invalid array', () => {
+      expect(is.array('')).toBeFalsy()
+    })
+
+    test('given an invalid array', () => {
+      expect(is.array('[]')).toBeFalsy()
     })
   })
 })
