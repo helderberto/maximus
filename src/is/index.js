0618397f47bfe60b34229964faf21ce67eaef929
Helder Burato Berto <helder.burato@gmail.com>
2020-08-29 19:33:50 -0300

    docs(is): add method explanation comments

--- src/is/index.js
+++ src/is/index.js
@@ -1,3 +1,9 @@
+/**
+ * @description Check the value data type
+ * @param {Object} targetPrototype - The instanceof object to be checked.
+ * @param {any} value - Value to be validated by targetPrototype.
+ * @returns {Boolean} - Returns true if value is the instance of targetPrototype.
+ */
 export function is(targetPrototype, value) {
   if (arguments.length === 1) {
     return (_value) => is(targetPrototype, _value)
