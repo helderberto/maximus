897b7ec4e629ca8ccbebcb17c031decdbbab057b
Helder Burato Berto <helder.burato@gmail.com>
2020-08-31 19:40:17 -0300

    docs(curry): improve method comments

--- src/curry/index.js
+++ src/curry/index.js
@@ -1,13 +1,28 @@
 /**
- * @description Create curried method
- * @param {Function} fn - Function to be curried.
- * @param {any} args - Rest of parameters to be passed to function.
- * @returns {Function} - Returns the curried method with arguments.
+ * @name curry
+ * @category Common Helpers
+ * @summary Create curried method.
+ *
+ * @description
+ * Create curried method.
+ *
+ * @param {Function} fn - the function to curry
+ * @param {Array} args - the rest of arguments
+ * @returns {Function} the curried method with arguments
+ *
+ * @example
+ * const sum = (a, b, c) => a + b + c
+ * const curriedSum = curry(sum)
+ * curriedSum(1)(2, 3)
+ * //=> 6
  */
-export default function curry(fn, args = []) {
-  return (..._args) =>
-    ((rest) => (rest.length >= fn.length ? fn(...rest) : curry(fn, rest)))([
-      ...args,
-      ..._args,
-    ])
+export default function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args)
+    }
+    return function (..._args) {
+      return curried.apply(this, args.concat(_args))
+    }
+  }
 }
