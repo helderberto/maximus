eef2442ea0a012176da550f90af1c81fc0105f80
Helder Burato Berto <helder.burato@gmail.com>
2020-08-31 18:37:03 -0300

    chore: set test environment to node

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['/node_modules/', 'dist'],
