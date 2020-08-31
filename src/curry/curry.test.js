0dde9cf0e743b21b92c8228a85c4d8988ebd3ace
Helder Burato Berto <helder.burato@gmail.com>
2020-08-31 13:36:14 -0300

    feat: add helper curry

---
+++ src/curry/curry.test.js
@@ -0,0 +1,16 @@
+import { curry } from '.'
+
+describe('maximus.curry', () => {
+  test('should validate curried numbers', () => {
+    const addTwoNumbers = (x, y) => x + y
+    const curriedAddTwoNumbers = curry(addTwoNumbers)
+
+    expect(curriedAddTwoNumbers(10, 5)).toEqual(15)
+  })
+
+  test('should test with more arguments', () => {
+    const add = curry((n1, n2) => n1 + n2)
+
+    expect(add(1, 2, 3)).toEqual(3)
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * from './is'
 export * from './isEmpty'
 export * from './string'
+export * from './curry'
