287bd9fd6c8aa90f2d3390ab02bc66ab2379969b
Helder Burato Berto <helder.burato@gmail.com>
2020-08-31 19:40:48 -0300

    docs(is): improve method comments

--- src/is/index.js
+++ src/is/index.js
@@ -2,15 +2,31 @@ import curry from '../curry'
 import isDefined from '../isDefined'
 
 /**
- * @description Check the value data type
- * @param {Object} targetPrototype - The instanceof object to be checked.
- * @param {any} value - Value to be validated by targetPrototype.
- * @returns {Boolean} - Returns true if value is the instance of targetPrototype.
+ * @name is
+ * @category Common Helpers
+ * @summary Is the given value instace of the target prototype?
+ *
+ * @description
+ * Checks if the given argument is from the correct type.
+ *
+ * @param {Object} targetPrototype - the prototype to check
+ * @param {*} value - the value to check
+ * @returns {Boolean} the value is with the correct type
+ *
+ * @example
+ * is(String, 'maximus')
+ * //=> true
+ *
+ * @example
+ * is(Array, {})
+ * //=> false
  */
-export default curry((targetPrototype, value) => {
+const is = curry((targetPrototype, value) => {
   if (!isDefined(value)) return false
 
   return (
     value.constructor === targetPrototype || value instanceof targetPrototype
   )
 })
+
+export default is
