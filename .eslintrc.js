3cc0f66e3e776dfc19ca9e5cc4b3edb239c38372
Ben Mezger (seds) <benmezger@users.noreply.github.com>
2020-09-25 11:46:13 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,1 @@
-module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-    jest: true,
-  },
-  extends: ['standard', 'prettier', 'prettier/standard'],
-  plugins: ['prettier', 'standard'],
-  parserOptions: {
-    sourceType: 'module',
-  },
-  rules: {
-    'prettier/prettier': 'error',
-  },
-}
+
