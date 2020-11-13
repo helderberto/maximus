c7c0b9352687a69cd915276767e04b285e8be97c
Helder Burato Berto <helder.burato@gmail.com>
2020-11-13 19:56:15 -0300

    refactor: restructure project
    This update is based on the issue #27

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 /* istanbul ignore file */
-export * from './curry'
-export * from './is'
-export * from './isDefined'
-export * from './isEmpty'
-export * from './capitalize'
+export * from './common'
