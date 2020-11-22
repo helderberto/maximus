59218aad734cbbfdf5a8f3c53876faf01b310f93
Helder Burato Berto <helder.burato@gmail.com>
2020-11-22 12:08:40 -0300

    refactor(curry): update curry.test.js to test.js

--- src/common/curry/test.js
+++ src/common/curry/test.js
@@ -1,13 +1,14 @@
 import { curry } from '.'
 
-describe('maximus.curry', () => {
-  test('should validate curried numbers', () => {
+describe('curry', () => {
+  it('should validate curried numbers', () => {
     const sum = (a, b, c) => a + b + c
     const curriedSum = curry(sum)
+
     expect(curriedSum(2)(2, 2)).toEqual(6)
   })
 
-  test('should test with more arguments', () => {
+  it('should test with more arguments', () => {
     const add = curry((n1, n2) => n1 + n2)
 
     expect(add(1, 2, 3)).toEqual(3)
