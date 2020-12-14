bcf5ada7860220d6f50028e0106a8f5a821e097a
Helder Burato Berto <helder.burato@gmail.com>
2020-12-14 12:20:46 -0300

    docs: add isEmpty common

--- src/common/is-empty/index.js
+++ src/common/is-empty/index.js
@@ -1,3 +1,16 @@
+/**
+ * It checks if value is empty
+ *
+ * @category Function
+ * @param {any} - value to be checked
+ * @returns {Boolean} returns boolean if value is empty
+ *
+ * @example
+ * isEmpty('') //=> true
+ * isEmpty({}) //=> true
+ * isEmpty([]) //=> true
+ * isEmpty() //=> true
+ */
 export const isEmpty = (value) => {
   return (
     value === null ||
