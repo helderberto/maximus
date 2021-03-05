c4535da0a3baf497c1da6ec395e8995adb90ef09
Helder Burato Berto <helder.burato@gmail.com>
2021-03-05 10:11:14 +0000

    style(prettier): remove prettier/standard
    It has been merged to prettier package

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['standard', 'prettier', 'prettier/standard'],
+  extends: ['standard', 'prettier'],
   plugins: ['prettier', 'standard'],
   parserOptions: {
     sourceType: 'module',
